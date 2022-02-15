401bd7587bb50193cfe24f7faaa3cd6d9d2c3914
cynthia.perez <cynthiaperezdaubert@gmail.com>
2022-02-15 18:41:13 -0300

    octvo commit, contact form arreglado

--- components/contact-form/contact-form.js
+++ components/contact-form/contact-form.js
@@ -18,15 +18,15 @@ function formComponent(elem) {
           <div class="fieldset">
               <label class="label__text" for="email">Email</label>
               <input id="email" class="input-text" name="email">
-            </div>
+          </div>
           <div class="fieldset">
               <label class="label__text" for="message">Mensaje</label>
               <textarea class="textarea" name="message"></textarea>
           </div>
 
-          <div class="button__container">
-              <button class="button">Enviar</button>
-          </div>
+          
+          <button class="button">Enviar</button>
+          
       </form>
     </div>
  `;
